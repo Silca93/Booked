81ac7c0db1ec39a57129c683a5f111c422e57635
Eric_PC_FIXE <edelhaye93@gmail.com>
2024-04-01 18:46:54 +0200

    fixing errors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ import { CgProfile } from "react-icons/cg";
 import { FaRegHeart } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa";
 
-import logo from './../assets/images/logos/BOOKED.png';
+import logo from './../assets/images/logos/booked.png';
 
 import { useState, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
