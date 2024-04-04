8aa0fa80132acd71731f51a6178b58f0a813c0fc
Eric_PC_FIXE <edelhaye93@gmail.com>
2024-04-04 22:31:39 +0200

    i hate responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -99,7 +99,7 @@ return (
                 </div>
             </div>
             <div className="Right w-[30%] h-full  rounded-r-full flex max-[768px]:w-[50%]">
-                <div className="w-[40%]  h-full flex gap-2 justify-end items-center max-[768px]:justify-center">
+                <div className="w-[40%]  h-full flex gap-2 justify-end items-center max-[768px]:justify-start">
                     <label className="cursor-pointer grid place-items-center max-[1200px]:hidden">
                         <input type="checkbox" value="synthwave" className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"/>
                         <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
@@ -146,7 +146,7 @@ return (
                     }
                         
                 <Link href="/Shopping">
-                <div className="flex gap-3 justify-center items-center w-[5rem] h-[2.5rem] bg-white rounded-md border-black border-[1px] max-[1400px]:w-[3.5rem] max-[1100px]:w-[2.5rem] max-[1100px]:h-[2rem] max-[768px]:w-[3.5rem] max-[768px]:h-[2.5rem]">
+                <div className="flex gap-3 justify-center items-center mr-3  w-[5rem] h-[2.5rem] bg-white rounded-md border-black border-[1px] max-[1400px]:w-[3.5rem] max-[1100px]:w-[2.5rem] max-[1100px]:h-[2rem] max-[768px]:w-[3.2rem] max-[768px]:h-[2.5rem]">
                     <div className="cart w-[65%] h-full flex  justify-end items-center">
                     <SlBasket size={25} className="max-[1400px]:w-[70%]" />
                     </div>
